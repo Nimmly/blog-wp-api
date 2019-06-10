8ae084c7e254217f65ea5894bfd96f2e02da4ccb
MarkoPop <marko.v.popovic@gmail.com>
2019-06-10 17:02:47 +0200

    search

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,9 +1,28 @@
 <template>
     <div>
-        <ul v-for="post in posts">
-            <li>{{ post.title.rendered }}</li>
-        </ul>
+        <section class="slantedDivA">
+            <div class="container">
+                <ul class="row">
+                    <li class="col-sm" v-for="post in posts">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ post.title.rendered }}</h5>
+                            <p class="card-text">{{ post.excerpt.rendered }}</p>
+                            <a href="#" class="btn btn-primary">CLick</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="sec2">
+            Footer text
+        </section>
+
+        <section class="section">
+
+        </section>
     </div>
+
+
 </template>
 
 <script>
@@ -18,3 +37,46 @@
 
     }
 </script>
+
+<style >
+
+ .slantedDivA{
+     position: relative;
+     width: 100%;
+     height: auto;
+     background: whitesmoke;
+     box-sizing: border-box;
+     padding: 30px;
+     color: black;
+     margin-bottom: 200px;
+ }
+
+ .slantedDivA:after{
+     position: absolute;
+     width: 100%;
+     height: 100%;
+     content: '';
+     background: inherit;
+     z-index: -1;
+     top: 0;
+     right: 0;
+     bottom: 0;
+     left: 0;
+     transform-origin: top right;
+     transform: skewY(-4deg);
+ }
+    body{
+        background: black;
+        color: white;
+    }
+    li{
+        list-style-type: none;
+    }
+    .card-body{
+        padding: 20px;
+        box-shadow: gray;
+    }
+
+
+</style>
+
---
+++ src/components/UploadForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <form  @submit="uploadPost" class="form-horizontal">
+            <div class="form-group">
+                <label for="title" class="control-label col-xs-4">Title</label>
+                <div class="col-xs-8">
+                    <div class="input-group">
+                        <div class="input-group-addon">
+                            <i class="fa fa-address-card"></i>
+                        </div>
+                        <input v-model="newPost.title" id="title" name="title" type="text" class="form-control" required minlength="2">
+                    </div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-xs-offset-4 col-xs-8">
+                    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                </div>
+                <div class="col-xs-offset-4 col-xs-8">
+                    <button name="reset" type="reset" class="btn btn-danger" >Reset</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+
+<script>
+    import { mapActions, mapGetters } from 'vuex';
+
+    export default {
+        name: 'UploadForm',
+        computed: mapGetters(['newPost']),
+        methods:mapActions(['uploadPost'])
+    }
+</script>
+
+<style>
+    body:after{
+        background:white;
+    }
+</style>
